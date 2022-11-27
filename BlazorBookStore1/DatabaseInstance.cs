03fce5e25e8c0aa5df51bc61b543660a230f09e1
brewstercm <90487199+brewstercm@users.noreply.github.com>
2022-11-27 17:37:53 -0600

    added DatabaseInstance class

---
+++ BlazorBookStore1/DatabaseInstance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorBookStore1
+{
+    public static class DatabaseInstance
+    {
+        private static string connectionString = @"Server=(LocalDB)\MSSQLLocalDB;Integrated Security=true;AttachDbFileName=C:\Users\Legen\Source\Repos\BookStore_Brewster_James\BlazorBookStore1\Resources\BookStoreDB.mdf;";
+
+    }
+}
