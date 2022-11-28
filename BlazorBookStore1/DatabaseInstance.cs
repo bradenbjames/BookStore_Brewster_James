344d1a2dd36796b5af221015a461c998ec3e5712
brewstercm <90487199+brewstercm@users.noreply.github.com>
2022-11-27 18:52:32 -0600

    added createAccount method to database instance

--- BlazorBookStore1/DatabaseInstance.cs
+++ BlazorBookStore1/DatabaseInstance.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace BlazorBookStore1
 {
@@ -10,5 +11,15 @@ namespace BlazorBookStore1
     {
         private static string connectionString = @"Server=(LocalDB)\MSSQLLocalDB;Integrated Security=true;AttachDbFileName=C:\Users\Legen\Source\Repos\BookStore_Brewster_James\BlazorBookStore1\Resources\BookStoreDB.mdf;";
 
+        public static void createAccount(string fName, string lName, string email, string password, bool isAdministrator)
+        {
+            string query = $"INSERT INTO dbo.Customer VALUES ('{fName}', '{lName}')";
+            using(SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, conn);
+                conn.Open();
+                command.ExecuteNonQuery();
+            }
+        }
     }
 }
